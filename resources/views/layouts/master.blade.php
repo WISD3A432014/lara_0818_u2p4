0cb5c4b5b392fe4fdf7a1d2ccdaf29fef5fd5168
Wu Hsiu Rong <mandy19970219@gm.student.ncut.edu.tw>
2017-10-26 18:10:03 +0800

    修改主樣板內script標籤js檔案的參考位置

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,4 +2,9 @@
     <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
     <link href="{{asset('css/clean-blog.min.css')}}" rel="stylesheet">
     <link href="{{asset('css/font-awesome.min.css')}}" rel="stylesheet" type="ext/css">
-    
+
+    <script scr="{{asset('js/jquery.min.js')}}"></script>
+    <script scr="{{asset('js/bootstrap.min.js')}}"></script>
+    <script scr="{{asset('js/jqBootstrapValidation.js')}}"></script>
+    <script scr="{{asset('js/contact_me.js')}}"></script>
+    <script scr="{{asset('js/clean-blog.min.js')}}"></script>
